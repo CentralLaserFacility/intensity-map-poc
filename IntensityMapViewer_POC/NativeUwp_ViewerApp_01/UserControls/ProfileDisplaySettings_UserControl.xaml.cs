a603def9d4eb6c4878bb016e3074da32f0e9309e
Steve Terepin <10615120+k563@users.noreply.github.com>
2021-02-01 12:22:02 +0000

    Skeleton of the visual structure for the main panel + user controls

---
+++ IntensityMapViewer_POC/NativeUwp_ViewerApp_01/UserControls/ProfileDisplaySettings_UserControl.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace NativeUwp_ViewerApp_01
+{
+  public sealed partial class ProfileDisplaySettings_UserControl : UserControl
+  {
+
+    private IntensityMapViewer.ProfileDisplaySettingsViewModel ViewModel ;
+
+    public ProfileDisplaySettings_UserControl()
+    {
+      this.InitializeComponent();
+    }
+
+  }
+
+}
