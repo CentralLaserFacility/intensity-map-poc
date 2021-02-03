c0d94a420ba2d835cc38501c47ba2f6a3bca8792
Steve Terepin <10615120+k563@users.noreply.github.com>
2021-02-03 15:20:08 +0000

    Image display drawing with both WritableBitmap and Skia

--- IntensityMapViewer_POC/NativeUwp_ViewerApp_01/UserControls/IntensityMapImage_UserControl.xaml.cs
+++ IntensityMapViewer_POC/NativeUwp_ViewerApp_01/UserControls/IntensityMapImage_UserControl.xaml.cs
@@ -37,7 +37,71 @@ namespace NativeUwp_ViewerApp_01
         // {
         //   m_textBox.Text = $"Just testing : {ViewModel.Dimensions.ToString()}" ;
         // }
+        m_skiaCanvas.PaintSurface += DrawSkiaContent ;
+
       } ;
     }
+
+    private void DrawSkiaContent ( 
+      object                                      sender, 
+      SkiaSharp.Views.UWP.SKPaintSurfaceEventArgs paintSurfaceEventArgs
+    ) {
+      SkiaSharp.SKImageInfo imageInfo = paintSurfaceEventArgs.Info ;
+      Common.DebugHelpers.WriteDebugLines(
+        $"SKImageInfo : size [{imageInfo.Width}x{imageInfo.Height}]"
+      ) ;
+      SkiaSharp.SKCanvas skiaCanvas = paintSurfaceEventArgs.Surface.Canvas ;
+      SkiaSharp.SKRectI deviceClipBounds = skiaCanvas.DeviceClipBounds ;
+      SkiaSharp.SKRect localClipBounds = skiaCanvas.LocalClipBounds ;
+      Common.DebugHelpers.WriteDebugLines(
+        $"Skia.Canvas.DeviceClipBounds : [{deviceClipBounds.Left},{deviceClipBounds.Top}] size [{deviceClipBounds.Width}x{deviceClipBounds.Height}]"
+      ) ;
+      Common.DebugHelpers.WriteDebugLines(
+        $"Skia.Canvas.LocalClipBounds : [{localClipBounds.Left},{localClipBounds.Top}] size [{localClipBounds.Width}x{localClipBounds.Height}]"
+      ) ;
+      paintSurfaceEventArgs.Surface.Canvas.DrawLine(
+        new SkiaSharp.SKPoint(
+          deviceClipBounds.Location.X,
+          deviceClipBounds.Location.Y
+        ),
+        new SkiaSharp.SKPoint(
+          deviceClipBounds.Location.X + deviceClipBounds.Width  - 1,
+          deviceClipBounds.Location.Y + deviceClipBounds.Height - 1
+        ),
+        new SkiaSharp.SKPaint(){
+          Color = SkiaSharp.SKColors.Red
+        }
+      ) ;
+      var intensityMap = ViewModel.MostRecentlyAcquiredIntensityMap ;
+      var bitmap = new SkiaSharp.SKBitmap(
+        intensityMap.Dimensions.Width,
+        intensityMap.Dimensions.Height
+      ) ;
+      bitmap.Pixels = intensityMap.IntensityValues.Select(
+        intensity => new SkiaSharp.SKColor(
+          red   : intensity,
+          green : intensity,
+          blue  : intensity
+        )
+      ).ToArray() ;
+      // SkiaSharp.SKRect rectInWhichToDrawBitmap = new SkiaSharp.SKRect(
+      //   left   : 100.0f,
+      //   top    : 10.0f,
+      //   right  : 100.0f + intensityMap.Dimensions.Width/2,
+      //   bottom : 10.0f  + intensityMap.Dimensions.Height/2 
+      // ) ;
+      SkiaSharp.SKRect rectInWhichToDrawBitmap = new SkiaSharp.SKRect(
+        left   : 0.0f,
+        top    : 0.0f,
+        right  : intensityMap.Dimensions.Width,
+        bottom : intensityMap.Dimensions.Height
+      ) ;
+      skiaCanvas.DrawBitmap(
+        bitmap,
+        rectInWhichToDrawBitmap
+      ) ;
+    }
+
   }
+
 }
