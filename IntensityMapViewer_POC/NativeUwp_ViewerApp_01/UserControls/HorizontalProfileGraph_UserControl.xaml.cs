76087e6026c3565e2be6e7d47769a313ddae08f3
Steve Terepin <10615120+k563@users.noreply.github.com>
2021-02-03 20:52:27 +0000

    Profile graph, rough and ready but it works

--- IntensityMapViewer_POC/NativeUwp_ViewerApp_01/UserControls/HorizontalProfileGraph_UserControl.xaml.cs
+++ IntensityMapViewer_POC/NativeUwp_ViewerApp_01/UserControls/HorizontalProfileGraph_UserControl.xaml.cs
@@ -13,6 +13,9 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
+using Common.ExtensionMethods ;
+using UwpSkiaUtilities;
+
 // The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
 
 namespace NativeUwp_ViewerApp_01
@@ -31,91 +34,59 @@ namespace NativeUwp_ViewerApp_01
         System.Diagnostics.Debug.WriteLine(
           $"{this.GetType()} DataContext => {DataContext?.GetType().ToString()??"null"}"
         ) ;
-        // this.Bindings.Update() ; // Yikes - gotta call this explicitly ? WTF !!!
+        // Yikes - gotta call this explicitly ? WTF !!!
+        // this.Bindings.Update() ; 
       } ;
-      m_skiaCanvas.PaintSurface += DrawSkiaContent ;
+      m_skiaCanvas.PaintSurface += (s,paintSurfaceEventArgs) => {
+        // UwpSkiaUtilities.DrawingHelpers.DrawBoundingBox(
+        //   paintSurfaceEventArgs
+        // ) ;
+        DrawHorizontalProfileGraph_IndividualLines(
+          paintSurfaceEventArgs.Surface.Canvas,
+          SkiaSharp.SKRect.Create(
+            new SkiaSharp.SKSize(
+              paintSurfaceEventArgs.Info.Width,
+              paintSurfaceEventArgs.Info.Height
+            )
+          ) 
+        ) ;
+      } ;
     }
 
-    private int m_offset = 1 ;
-
-    private void DrawSkiaContent ( 
-      object                                      sender, 
-      SkiaSharp.Views.UWP.SKPaintSurfaceEventArgs paintSurfaceEventArgs
+    private void DrawHorizontalProfileGraph_IndividualLines (
+      SkiaSharp.SKCanvas skiaCanvas,
+      SkiaSharp.SKRect   canvasRect
     ) {
-      SkiaSharp.SKImageInfo imageInfo = paintSurfaceEventArgs.Info ;
-      Common.DebugHelpers.WriteDebugLines(
-        $"SKImageInfo : size [{imageInfo.Width}x{imageInfo.Height}]"
+      var red = new SkiaSharp.SKPaint(){
+        Color = SkiaSharp.SKColors.Red
+      } ;
+      canvasRect.UnpackVisibleCornerPoints(
+        out SkiaSharp.SKPoint topLeftPoint,    
+        out SkiaSharp.SKPoint topRightPoint,   
+        out SkiaSharp.SKPoint bottomLeftPoint, 
+        out SkiaSharp.SKPoint bottomRightPoint
       ) ;
-      SkiaSharp.SKCanvas skiaCanvas = paintSurfaceEventArgs.Surface.Canvas ;
-      SkiaSharp.SKRectI deviceClipBounds = skiaCanvas.DeviceClipBounds ;
-      SkiaSharp.SKRect localClipBounds = skiaCanvas.LocalClipBounds ;
-      Common.DebugHelpers.WriteDebugLines(
-        $"Skia.Canvas.DeviceClipBounds : [{deviceClipBounds.Left},{deviceClipBounds.Top}] size [{deviceClipBounds.Width}x{deviceClipBounds.Height}]"
-      ) ;
-      Common.DebugHelpers.WriteDebugLines(
-        $"Skia.Canvas.LocalClipBounds : [{localClipBounds.Left},{localClipBounds.Top}] size [{localClipBounds.Width}x{localClipBounds.Height}]"
-      ) ;
-      paintSurfaceEventArgs.Surface.Canvas.DrawRect(
-        deviceClipBounds.Location.X,
-        deviceClipBounds.Location.Y,
-        deviceClipBounds.Location.X + deviceClipBounds.Width,
-        deviceClipBounds.Location.Y + deviceClipBounds.Height,
-        new SkiaSharp.SKPaint(){
-          Color = SkiaSharp.SKColors.Blue
-        }
-      ) ;
-      paintSurfaceEventArgs.Surface.Canvas.DrawRect(
-        deviceClipBounds.Location.X + m_offset,
-        deviceClipBounds.Location.Y + m_offset,
-        deviceClipBounds.Location.X + deviceClipBounds.Width  - 1 - m_offset,
-        deviceClipBounds.Location.Y + deviceClipBounds.Height - 1 - m_offset,
-        new SkiaSharp.SKPaint(){
-          Color = SkiaSharp.SKColors.LightGreen
-        }
-      ) ;
-      // Vertical line at the left, down from the top
-      paintSurfaceEventArgs.Surface.Canvas.DrawLine(
-        new SkiaSharp.SKPoint(
-          // Top left
-          deviceClipBounds.Location.X,
-          deviceClipBounds.Location.Y
-        ),
-        new SkiaSharp.SKPoint(
-          // Top left, down 20
-          deviceClipBounds.Location.X, 
-          deviceClipBounds.Location.Y + 20
-        ),
-        new SkiaSharp.SKPaint(){
-          Color = SkiaSharp.SKColors.Red
-        }
-      ) ;
-      // Vertical line at the right, up from the bottom
-      paintSurfaceEventArgs.Surface.Canvas.DrawLine(
-        new SkiaSharp.SKPoint(
-          // Bottom right
-          deviceClipBounds.Location.X + deviceClipBounds.Width  - 1,
-          deviceClipBounds.Location.Y + deviceClipBounds.Height - 1
-        ),
-        new SkiaSharp.SKPoint(
-          // Bottom right, up 20
-          deviceClipBounds.Location.X + deviceClipBounds.Width  - 1,
-          deviceClipBounds.Location.Y + deviceClipBounds.Height - 1 - 20
-        ),
-        new SkiaSharp.SKPaint(){
-          Color = SkiaSharp.SKColors.Red
-        }
-      ) ;
-      paintSurfaceEventArgs.Surface.Canvas.DrawLine(
-        new SkiaSharp.SKPoint(
-          deviceClipBounds.Location.X,
-          deviceClipBounds.Location.Y
-        ),
-        new SkiaSharp.SKPoint(
-          deviceClipBounds.Location.X + deviceClipBounds.Width  - 1,
-          deviceClipBounds.Location.Y + deviceClipBounds.Height - 1
-        ),
-        new SkiaSharp.SKPaint(){
-          Color = SkiaSharp.SKColors.Red
+      float spaceAtTopAndBottom = 4.0f ;
+      int nPoints = ViewModel.MostRecentlyAcquiredIntensityMap.HorizontalSliceAtRow(0).Count ;
+      ViewModel.MostRecentlyAcquiredIntensityMap.HorizontalSliceAtRow(120).ForEachItem(
+        (value,i) => {
+          float lineHeight = (
+            (
+              canvasRect.Height 
+            - spaceAtTopAndBottom * 2.0f
+            )
+          * value / 255.0f 
+          ) ;
+          var bottomPoint = UwpSkiaUtilities.DrawingHelpers.GetPointAtFractionalPositionAlongLine(
+            bottomLeftPoint.MovedBy(0,-spaceAtTopAndBottom),
+            bottomRightPoint.MovedBy(0,-spaceAtTopAndBottom),
+            i / (float) nPoints
+          ) ;
+          skiaCanvas.DrawVerticalLineUp(
+            bottomPoint,
+            lineHeight,
+            red
+          ) ;
         }
       ) ;
     }
