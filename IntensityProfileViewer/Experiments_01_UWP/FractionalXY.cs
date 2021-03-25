d21c71347faf437d0c746dc1373ba8552c31ac05
Steve Terepin <10615120+k563@users.noreply.github.com>
2021-03-25 14:39:27 +0000

    Safety copy

--- IntensityProfileViewer/Experiments_01_UWP/FractionalXY.cs
+++ IntensityProfileViewer/Experiments_01_UWP/FractionalXY.cs
@@ -8,9 +8,18 @@ namespace Experiments_01_UWP
   public record FractionalXY ( double X, double Y )
   {
 
+    public static readonly FractionalXY Zero = new FractionalXY(0.0,0.0) ;
+
     public bool IsZero => ( 
        X == 0.0 
     && Y == 0.0 
+    ) ;
+
+    public bool IsInsideNominalBounds => (
+       X >= 0.0
+    && X <= 1.0
+    && Y >= 0.0
+    && Y <= 1.0
     ) ;
 
     public double Length => System.Math.Sqrt(LengthSquared) ;
@@ -23,6 +32,18 @@ namespace Experiments_01_UWP
     ) ;
 
     public override string ToString ( ) => $"[{X:F3},{Y:F3}]" ;
+
+    public static FractionalXY operator + ( FractionalXY from, FractionalXY to )
+    => new FractionalXY(
+      to.X * from.X,
+      to.Y * from.Y
+    ) ;
+
+    public static FractionalXY operator - ( FractionalXY from, FractionalXY to )
+    => new FractionalXY(
+      to.X - from.X,
+      to.Y - from.Y
+    ) ;
 
   }
 
