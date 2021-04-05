77bbde1410f956d646d1a5488e9517bfc5985bd4
Steve Terepin <10615120+k563@users.noreply.github.com>
2021-04-05 18:53:41 +0100

    Reworking the top level Viewer XAML - 'flat' version

---
+++ IntensityProfileViewer/IntensityProfileViewer/IntensityProfileViewer.Shared/UserControls/ViewerPanel_flat_UserControl.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace IntensityProfileViewer
+{
+
+  public sealed partial class ViewerPanel_flat_UserControl : UserControl
+  {
+
+    public static readonly DependencyProperty ViewModelProperty = DependencyProperty.Register(
+      "ViewModel", 
+      typeof(IntensityProfileViewer.IDisplayPanelViewModel), 
+      typeof(ViewerPanel_flat_UserControl), 
+      new PropertyMetadata(0)
+    ) ;
+
+    public IntensityProfileViewer.IDisplayPanelViewModel ViewModel
+    {
+      get => GetValue(ViewModelProperty) as IntensityProfileViewer.IDisplayPanelViewModel ;
+      set => SetValue(ViewModelProperty,value) ;
+    }
+
+    public ViewerPanel_flat_UserControl ( )
+    {
+      this.InitializeComponent() ;
+    }
+
+    private void ResetPanZoomButton_Click ( object sender, RoutedEventArgs e )
+    {
+      // HACK ...
+      // m_intensityMapImageAndProfileGraphs_UserControl.ResetPanZoomButton_Click(sender,e) ;
+    }
+
+  }
+
+}
