1a429969acc45bcd22727011c7d19689b9c32c05
Steve Terepin <10615120+k563@users.noreply.github.com>
2021-04-28 16:07:13 +0100

    Sketch of 'EpicsImageSource' API

---
+++ IntensityProfileViewer/ModelClasses/EpicsImageSource.cs
@@ -0,0 +1,37 @@
+﻿//
+// EpicsImageSource.cs
+//
+
+namespace IntensityProfileViewer
+{
+
+  public record ImageDataFromEpics ( 
+    int    Height,
+    int    Width,
+    byte[] ImageBytes
+  ) ;
+
+  public interface IEpicsImageSource : System.IDisposable
+  {
+    System.Action<ImageDataFromEpics>? NewImageAvailable { get ; set ; }
+  }
+
+  public class EpicsImageSource : IEpicsImageSource
+  {
+
+    public System.Action<ImageDataFromEpics>? NewImageAvailable { get ; set ; }
+
+    public EpicsImageSource ( string pvBaseName ) // Might need separate PV names for height/width/data ??
+    {
+      // Connect to the PV's, and set up callbacks that tell us when a new image is available ...
+    }
+
+    public void Dispose ( )
+    {
+      // Close down the connections etc ...
+      throw new System.NotImplementedException() ;
+    }
+
+  }
+    
+}
